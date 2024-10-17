a11a0f5a190a42981bdb1eee3877540f08d46271
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-10-17 13:26:20 +0200

    start Article component

---
+++ src/components/article.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function Article() {
+  const [nb_article, setNbArticle] = useState(1);
+  const stock = 5;
+  function add_one(){
+      if (nb_article < stock) {
+          setNbArticle(nb_article + 1);
+      }else{}
+  }
+  function remove_one(){
+      if (nb_article > 0){
+          setNbArticle(nb_article - 1);
+      }else{}
+  }
+  function buy(){
+      console.log(`added to the basket ${nb_article} times`);
+  }
+  return (
+    <div className="w-full h-full bg-gray-100 rounded-lg">
+        <img className="w-full rounded-t-lg" src="https://placehold.co/150x150" />
+        <div className="w-full h-16 flex flex-row justify-between">
+            <div className="w-5/12">
+                <p className="ml-1">Pomme</p>
+                <div className="flex flex-row justify-end divide-x divide-gray-100">
+                    <button className="bg-gray-200 w-8 h-8 text-xl text-center rounded-l-lg" onClick={remove_one}>-</button>
+                    <p className="bg-gray-200 w-8 h-8 text-xl text-center">{ nb_article }</p>
+                    <button className="bg-gray-200 w-8 h-8 text-xl text-center rounded-r-lg" onClick={add_one}>+</button>
+                </div>
+            </div>
+            <div className="flex flex-col justify-around w-5/12">
+                <p className="mr-1 text-end">restant: { stock }</p>
+                <button className="bg-green-300 text-xl text-center rounded-lg" onClick={buy}>Acheter</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Article;
