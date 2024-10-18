0d6eaf83c2fcfa1fd8d0f98800419254d9de8891
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-10-18 12:37:58 +0200

    field validation

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { article } from "../articleType";
 
 enum BasketState {
@@ -8,7 +8,23 @@ enum BasketState {
   valided,
 }
 
+enum FieldValidity {
+  valid,
+  not_valid,
+  not_looked,
+}
+
 function BasketArticleList({basket_state, SetShowingBasket, basket, setBasket}: {basket_state: BasketState, SetShowingBasket: (v:BasketState)=>undefined, basket: Array<{article: article, quantity: number}>, setBasket(v:Array<{article: article, quantity: number}>):void}){
+    const [first_name, setFirstName] = useState<string>("");
+    const [first_name_validity, setFirstNameValidity] = useState<FieldValidity>(FieldValidity.not_looked);
+    const [last_name, setLastName] = useState<string>("");
+    const [last_name_validity, setLastNameValidity] = useState<FieldValidity>(FieldValidity.not_looked);
+    const [email, setEmail] = useState<string>("");
+    const [email_validity, setEmailValidity] = useState<FieldValidity>(FieldValidity.not_looked);
+    const [adresse, setAdresse] = useState<string>("");
+    const [adresse_validity, setAdresseValidity] = useState<FieldValidity>(FieldValidity.not_looked);
+    const [credit_card, setCreditCard] = useState<string>("");
+    const [credit_card_validity, setCreditCardValidity] = useState<FieldValidity>(FieldValidity.not_looked);
     function get_total_price(): number{
         let total_price = 0;
         for (let i=0;i<basket.length;i++){
@@ -17,9 +33,50 @@ function BasketArticleList({basket_state, SetShowingBasket, basket, setBasket}: 
         return total_price;
     }
 
-    function validate_transaction(): void{
-        setBasket([]);
-        SetShowingBasket(BasketState.valided);
+    function validate_transaction(e:FormEvent<HTMLFormElement>): void{
+        e.preventDefault();
+        let is_valid = true;
+        // first name
+        if (first_name.trim().length < 3 || first_name.trim().length > 40){
+            setFirstNameValidity(FieldValidity.not_valid);
+            is_valid = false;
+        }else{
+            setFirstNameValidity(FieldValidity.valid);
+        }
+        // last name
+        if (last_name.trim().length < 3 || last_name.trim().length > 40){
+            setLastNameValidity(FieldValidity.not_valid);
+            is_valid = false;
+        }else{
+            setLastNameValidity(FieldValidity.valid);
+        }
+        // email
+        if (!/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/.test(email)){
+            setEmailValidity(FieldValidity.not_valid);
+            is_valid = false;
+        }else{
+            setEmailValidity(FieldValidity.valid);
+        }
+        // adresse
+        if (!adresse.trim()){
+            setAdresseValidity(FieldValidity.not_valid);
+            is_valid = false;
+        }else{
+            setAdresseValidity(FieldValidity.valid);
+        }
+        // credit card
+        if (!/^\d+$/.test(credit_card)){
+            setCreditCardValidity(FieldValidity.not_valid);
+            is_valid = false;
+        }else{
+            setCreditCardValidity(FieldValidity.valid);
+        }
+        if (is_valid){
+            setCreditCard("");
+            setCreditCardValidity(FieldValidity.not_looked);
+            setBasket([]);
+            SetShowingBasket(BasketState.valided);
+        }
     }
 
     if (basket_state == BasketState.hidden){
@@ -73,15 +130,45 @@ function BasketArticleList({basket_state, SetShowingBasket, basket, setBasket}: 
                 <div className="ms-auto mt-5">
                     <form className="mx-auto w-[70vw] flex flex-col content-end">
                         <div className="flex flex-row justify-between">
-                            <input className="w-5/12 rounded p-1 shadow border border-2 text-gray-700 leading-tight focus:border-sky-200 focus:outline-none focus:shadow-outline" type="text" name="first-name" placeholder="prénom" />
-                            <input className="w-5/12 rounded p-1 shadow border border-2 text-gray-700 leading-tight focus:border-sky-200 focus:outline-none focus:shadow-outline" type="text" name="last-name" placeholder="nom" />
+                            {first_name_validity == FieldValidity.not_valid ? (
+                                <input value={first_name} onChange={(e)=>setFirstName(e.target.value)} className="w-5/12 rounded p-1 shadow border border-4 border-red-200 text-gray-700 leading-tight focus:border-sky-200 outline-none shadow-outline" type="text" name="first-name" placeholder="prénom" />
+                            ) : first_name_validity == FieldValidity.valid ? (
+                                <input value={first_name} onChange={(e)=>setFirstName(e.target.value)} className="w-5/12 rounded p-1 shadow border border-4 border-green-200 text-gray-700 leading-tight focus:border-sky-200 outline-none shadow-outline" type="text" name="first-name" placeholder="prénom" />
+                            ) : (
+                                <input value={first_name} onChange={(e)=>setFirstName(e.target.value)} className="w-5/12 rounded p-1 shadow border border-2 text-gray-700 leading-tight focus:border-sky-200 focus:outline-none focus:shadow-outline" type="text" name="first-name" placeholder="prénom" />
+                            )}
+                            {last_name_validity == FieldValidity.not_valid ? (
+                                <input value={last_name} onChange={(e)=>setLastName(e.target.value)} className="w-5/12 rounded p-1 shadow border border-4 border-red-200 text-gray-700 leading-tight focus:border-sky-200 outline-none shadow-outline" type="text" name="last-name" placeholder="nom" />
+                            ) : last_name_validity == FieldValidity.valid ? (
+                                <input value={last_name} onChange={(e)=>setLastName(e.target.value)} className="w-5/12 rounded p-1 shadow border border-4 border-green-200 text-gray-700 leading-tight focus:border-sky-200 outline-none shadow-outline" type="text" name="last-name" placeholder="nom" />
+                            ) : (
+                                <input value={last_name} onChange={(e)=>setLastName(e.target.value)} className="w-5/12 rounded p-1 shadow border border-2 text-gray-700 leading-tight focus:border-sky-200 focus:outline-none focus:shadow-outline" type="text" name="last-name" placeholder="nom" />
+                            )}
                         </div>
                         <br />
-                        <input className="rounded p-1 shadow border border-2 text-gray-700 leading-tight focus:border-sky-200 focus:outline-none focus:shadow-outline" type="email" name="email" placeholder="email" />
+                        {email_validity == FieldValidity.not_valid ? (
+                            <input value={email} onChange={(e)=>setEmail(e.target.value)} className="rounded p-1 shadow border border-4 border-red-200 text-gray-700 leading-tight focus:border-sky-200 outline-none shadow-outline" type="email" name="email" placeholder="email" />
+                        ) : email_validity == FieldValidity.valid ? (
+                            <input value={email} onChange={(e)=>setEmail(e.target.value)} className="rounded p-1 shadow border border-4 border-green-200 text-gray-700 leading-tight focus:border-sky-200 outline-none shadow-outline" type="email" name="email" placeholder="email" />
+                        ) : (
+                            <input value={email} onChange={(e)=>setEmail(e.target.value)} className="rounded p-1 shadow border border-2 text-gray-700 leading-tight focus:border-sky-200 focus:outline-none focus:shadow-outline" type="email" name="email" placeholder="email" />
+                        )}
                         <br />
-                        <input className="rounded p-1 shadow border border-2 text-gray-700 leading-tight focus:border-sky-200 focus:outline-none focus:shadow-outline" type="text" name="adresse" placeholder="adresse" />
+                        {adresse_validity == FieldValidity.not_valid ? (
+                            <input value={adresse} onChange={(e)=>setAdresse(e.target.value)} className="rounded p-1 shadow border border-4 border-red-200 text-gray-700 leading-tight focus:border-sky-200 outline-none shadow-outline" type="text" name="adresse" placeholder="adresse" />
+                        ) : adresse_validity == FieldValidity.valid ? (
+                            <input value={adresse} onChange={(e)=>setAdresse(e.target.value)} className="rounded p-1 shadow border border-4 border-green-200 text-gray-700 leading-tight focus:border-sky-200 outline-none shadow-outline" type="text" name="adresse" placeholder="adresse" />
+                        ) : (
+                            <input value={adresse} onChange={(e)=>setAdresse(e.target.value)} className="rounded p-1 shadow border border-2 text-gray-700 leading-tight focus:border-sky-200 focus:outline-none focus:shadow-outline" type="text" name="adresse" placeholder="adresse" />
+                        )}
                         <br />
-                        <input className="rounded p-1 shadow border border-2 text-gray-700 leading-tight focus:border-sky-200 focus:outline-none focus:shadow-outline" type="text" name="credit-card" placeholder="numéro de carte de crédit" />
+                        {credit_card_validity == FieldValidity.not_valid ? (
+                            <input value={credit_card} onChange={(e)=>setCreditCard(e.target.value)} className="rounded p-1 shadow border border-4 border-red-200 text-gray-700 leading-tight focus:border-sky-200 outline-none shadow-outline" type="text" name="credit-card" placeholder="numéro de carte de crédit" />
+                        ) : credit_card_validity == FieldValidity.valid ? (
+                            <input value={credit_card} onChange={(e)=>setCreditCard(e.target.value)} className="rounded p-1 shadow border border-4 border-green-200 text-gray-700 leading-tight focus:border-sky-200 outline-none shadow-outline" type="text" name="credit-card" placeholder="numéro de carte de crédit" />
+                        ) : (
+                            <input value={credit_card} onChange={(e)=>setCreditCard(e.target.value)} className="rounded p-1 shadow border border-2 text-gray-700 leading-tight focus:border-sky-200 focus:outline-none focus:shadow-outline" type="text" name="credit-card" placeholder="numéro de carte de crédit" />
+                        )}
                         <br />
                         <button className="p-3 bg-green-300 hover:bg-green-400 text-xl text-center rounded-lg ml-auto" onClick={validate_transaction}>Acheter</button>
                     </form>
